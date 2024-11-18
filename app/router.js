33cb1f5557ebe6ce4698886da818da35b8bea2e1
Sree <sbcards@gmail.com>
2024-11-17 21:04:46 -0700

    Headers

--- app/router.js
+++ app/router.js
@@ -1,9 +1,14 @@
 import EmberRouter from '@ember/routing/router';
-import config from 'my-ember-project/config/environment';
+import config from 'my-ember-app/config/environment';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('about');
+  this.route('posts', function () {
+    this.route('show', { path: '/:post_id' });
+  });
+});
